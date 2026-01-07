24c5e0c785177b9f903336264b218abc439549e3
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2026-01-06 19:52:06 -0800

    #7: Complete tabular reading

--- Ry/Modules/IOLayer.py
+++ Ry/Modules/IOLayer.py
@@ -1,4 +1,3 @@
-import os as _os
 import pandas as _pd
 
 from .utils.typing_helpers import copy_callable_signature
@@ -14,6 +13,8 @@ def read_csv(filepath_or_buffer, /, **kwargs) -> _pd.DataFrame:
         pd.DataFrame: The DataFrame containing the CSV data.
     """
     kwargs.pop("filepath_or_buffer", None) # Remove if present to avoid conflicts
+    kwargs.pop("delimiter", None)  # Remove delimiter if present to avoid conflicts
+    kwargs["sep"] = ","  # Set separator to comma for CSV files
     return _pd.read_csv(filepath_or_buffer, **kwargs)
 
 
@@ -28,5 +29,34 @@ def read_tsv(filepath_or_buffer, /, **kwargs) -> _pd.DataFrame:
         pd.DataFrame: The DataFrame containing the TSV data.
     """
     kwargs.pop("filepath_or_buffer", None) # Remove if present to avoid conflicts
+    kwargs.pop("delimiter", None)  # Remove delimiter if present to avoid conflicts
     kwargs["sep"] = "\t"  # Set separator to tab for TSV files
     return _pd.read_csv(filepath_or_buffer, **kwargs)
+
+
+@copy_callable_signature(_pd.read_csv)
+def read_txt(filepath_or_buffer, /, **kwargs) -> _pd.DataFrame:
+    """Reads a TXT file and returns a pandas DataFrame.
+
+    Args:
+        filepath_or_buffer (os.PathLike): The path to the TXT file.
+        **kwargs: Additional keyword arguments to pass to pandas read_csv.
+    Returns:
+        pd.DataFrame: The DataFrame containing the TXT data.
+    """
+    kwargs.pop("filepath_or_buffer", None) # Remove if present to avoid conflicts
+    return _pd.read_csv(filepath_or_buffer, **kwargs)
+
+
+@copy_callable_signature(_pd.read_fwf)
+def read_fwf(filepath_or_buffer, /, **kwargs) -> _pd.DataFrame:
+    """Reads a fixed-width formatted file and returns a pandas DataFrame.
+
+    Args:
+        filepath_or_buffer (os.PathLike): The path to the fixed-width formatted file.
+        **kwargs: Additional keyword arguments to pass to pandas read_fwf.
+    Returns:
+        pd.DataFrame: The DataFrame containing the fixed-width formatted data.
+    """
+    kwargs.pop("filepath_or_buffer", None) # Remove if present to avoid conflicts
+    return _pd.read_fwf(filepath_or_buffer, **kwargs)
